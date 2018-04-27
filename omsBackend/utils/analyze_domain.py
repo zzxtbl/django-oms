6ea459d93d3f2897049b343918f64278b13dbc6f
kevin <kevin@126.com>
2018-04-27 13:58:06 +0800

    add log

--- omsBackend/utils/analyze_domain.py
+++ omsBackend/utils/analyze_domain.py
@@ -16,21 +16,14 @@ def initlog(logfile):
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
-    logger.setLevel(logging.WARNING)
+    logger.setLevel(logging.INFO)
     return logger
 
 
-def diffdns(alldomains):
+def diffdns(alldomains, domainstatus_url, record_url):
     domains = json.loads(requests.get(alldomains).text)
     for domain in domains:
-        d = datetime.now()
-        d10 = d - timedelta(minutes=10)
-        cur_date = '{}-{}-{}'.format(d.year, d.month, d.day)
-        cur_time = '{}:{}:{}'.format(d.hour, d.minute, d.second)
-        create_time = '{}:{}:{}'.format(d10.hour, d10.minute, d10.second)
-        uu = 'http://118.193.136.206:8000/api/domainstatus/?domain={}&create_time_0={}&create_time_1={}'.format(domain, cur_date, create_time, cur_time)
-        # uu = 'http://118.193.136.206:8000/api/domainstatus/?domain={}'.format(domain)
-        urlinfos = json.loads(requests.get(uu).text)
+        urlinfos = json.loads(requests.get(domainstatus_url + domain).text)
         if not len(urlinfos):
             logging.error("%s - 收集的状态记录为空" % domain)
             continue
@@ -49,13 +42,11 @@ def diffdns(alldomains):
 
         if len(ss) > result['node_count'] / 2:
             result['status'] = True
+            logging.info("%s - [域名正常，不需要变换ip] - %s" % (domain, result))
         else:
             result['status'] = False
 
             # 自动切换ip
-            # record_url = 'http://oms.tb-gaming.local/api/dnsrecords/'
-            record_url = 'http://127.0.0.1:8000/api/dnsrecords/'
-
             x = domain.split('.')
             domainname = '{}.{}'.format(x[1], x[2])
             recordname = x[0]
@@ -67,13 +58,25 @@ def diffdns(alldomains):
                 recorddata['value'], recorddata['value2'] = recorddata['value2'], recorddata['value']
                 put_url = '{}{}/'.format(record_url, recorddata['id'])
                 requests.put(put_url, data=recorddata)
-                logging.warning("%s - ip已经自动更换 - %s" % (domain, result))
+                logging.warning("%s - [域名异常，ip已经自动更换] - %s" % (domain, result))
             else:
-                logging.error("%s - 没有设置备用ip - %s" % (domain, result))
+                logging.error("%s - [域名异常，没有设置备用ip] - %s" % (domain, result))
+    logging.info("轮回结束，等待下一次...")
     return
 
 
 if __name__ == '__main__':
+    initlog('./tan.log')
+    logging.info("命运的齿轮再次转动，轮回开始...")
+
     alldomains = 'http://118.193.136.206:8000/api/alldomains/'
-    initlog('../logs/tan.log')
-    print(diffdns(alldomains))
+    d = datetime.now()
+    d10 = d - timedelta(minutes=10)
+    cur_date = '{}-{}-{}'.format(d.year, d.month, d.day)
+    cur_time = '{}:{}:{}'.format(d.hour, d.minute, d.second)
+    create_time = '{}:{}:{}'.format(d10.hour, d10.minute, d10.second)
+    # domainstatus_url = 'http://118.193.136.206:8000/api/domainstatus/?create_time_0={}&create_time_1={}&domain='.format(cur_date, create_time, cur_time)
+    # record_url = 'http://oms.tb-gaming.local/api/dnsrecords/'
+    domainstatus_url = 'http://118.193.136.206:8000/api/domainstatus/?domain='
+    record_url = 'http://127.0.0.1:8000/api/dnsrecords/'
+    print(diffdns(alldomains, domainstatus_url, record_url))
