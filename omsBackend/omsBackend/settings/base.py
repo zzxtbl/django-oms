2dcdcbb802d186343b63e3825d234e81585dc887
kevin <kevin@126.com>
2018-04-30 16:46:16 +0800

    fix fileupload

--- omsBackend/omsBackend/settings/base.py
+++ omsBackend/omsBackend/settings/base.py
@@ -40,11 +40,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'utils.django.DisableCSRF',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
