4d27fca00129b431f8ad00ec7e29bb7a0e39e540
kevin <kevin@126.com>
2018-05-01 17:21:11 +0800

    add zabbix api

--- omsBackend/utils/zabbix_conf.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# author: itimor
-
-zabbix_info = {
-    'apiurl': 'http://zabbix.tbsysmanager.com/api_jsonrpc.php',
-    'username': 'kiven',
-    'password': 'la37ZjKVSANUFJIx'
-}
