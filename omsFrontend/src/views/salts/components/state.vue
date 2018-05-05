30faefcf42c391fbb996629eafbfc9dc852e6996
kevin <kevin@126.com>
2018-05-05 18:42:48 +0800

    add runstate vue

--- omsFrontend/src/views/salts/components/state.vue
+++ omsFrontend/src/views/salts/components/state.vue
@@ -3,7 +3,7 @@
     <el-card>
       <div class="head-lavel">
         <div class="table-button">
-          <el-button type="danger">state服务</el-button>
+          <el-button type="success">state服务</el-button>
           <el-button type="primary" icon="el-icon-plus" @click="addForm=true">新建</el-button>
         </div>
         <div class="table-search">
